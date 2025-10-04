abcbe8e7bd7df3d7e7866ad6fa431ed4a84ce566
matheus <matheus.silveiraw@gmail.com>
2025-10-04 15:45:55 -0300

    apagado comentário

--- app/Models/DeskRentalsModel.php
+++ app/Models/DeskRentalsModel.php
@@ -8,11 +8,5 @@ class DeskRentalsModel extends Model
 {
     protected $table = 'deskRentals';
     protected $primaryKey = 'idDeskRental';
-    protected $allowedFields = ['idDesk', 'idCustomer', 'startPeriod', 'endPeriod'];
-
-    // idDeskRental INT AUTO_INCREMENT PRIMARY KEY,
-    // idDesk INT NOT NULL,
-    // idCustomer INT NOT NULL,
-    // startPeriod DATETIME NOT NULL,
-    // endPeriod DATETIME NOT NULL,
+    protected $allowedFields = ['idDesk', 'idCustomer', 'idPlan', 'startPeriod', 'endPeriod', 'total_price'];
 }
